7cf273528c2d3fbb0a6e25966d102af75640d498
Xinjiang Shao <xinjiang.shao@ahold.com>
2017-10-25 22:14:39 -0500

    Add my wish list and night mode basic for the extension.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # refined-peapod
-An opinionated way of using Peapod.
+An opinionated way of using Peapod. I'm not saying this is the only way,
+but I do like cleaner design rather than complex.
+
+### UI Improvements
+- [ ] Night Mode Support
+- [ ] Clean Category Tree and Remove Icons
+- [ ] Simply Nav and Remove The menu item that I don't use often
+- [ ] Remove Notification Bar and Hi Message
+- [ ] Better format for Item Detail Description so that I can actually read.
+- [ ] Remove all sales flags to make UI cleaner
+
+### Function Improvements
+- [ ] Linkify the content when I see one
+- [ ] Quick Search in Google, Amazon, Instacart
+- [ ] If I cannot find my grocery here, I need to quick way to find it somewhere else
+- [ ] If I want peapod to carry the item I want, I want to provide an easy way to add the item. Expose it to the top navigation bar
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: {
+        content: './src/content',
+        background: './src/background',
+        options: './src/options'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            process: '0'
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ],
+    output: {
+        path: path.join(__dirname, 'extension'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new UglifyJSPlugin({
+            sourceMap: true,
+            uglifyOptions: {
+                mangle: false,
+                output: {
+                    beautify: true
+                }
+            }
+        })
+    );
+} else {
+    module.exports.devtool = 'source-map';
+}
